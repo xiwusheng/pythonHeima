3c6413ae0ae1ffaa70f29f108c40d0b308904175
ubuntu18.04YL <ubuntu1804@foxmail.com>
2019-08-05 07:56:22 +0800

    fixed some bug,late will do more

--- dailyfresh/df_cart/views.py
+++ dailyfresh/df_cart/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from .models import *
 from df_user import user_decorator
 from django.shortcuts import render, redirect
+from df_goods.models import *
 
 # Create your views here.
 
@@ -32,21 +33,34 @@ def add(request, gid, count):
     gid, count = int(gid), int(count)
     # 查询购物车中是否已经有此物品，如果有则数量增加，没有则新增
     carts = CartInfo.objects.filter(user_id=uid, goods_id=gid)
+    goods = GoodsInfo.objects.filter(id=gid)
 
     if len(carts) >= 1:
         cart = carts[0]
-        cart.count = cart.count + count
+        a1 = cart.count + count
+        # 这里为啥需要用goods[0]
+        if a1 > float(goods[0].gkucun):
+            cart.count = goods[0].gkucun
+        else:
+            cart.count = a1
     else:
+
         cart = CartInfo()
         cart.user_id = uid
         cart.goods_id = gid
-        cart.count = count
+
+        # 后台再次验证用户提交数量应该小于库存
+        if count > float(goods[0].gkucun):
+            cart.count = goods[0].gkucun
+        else:
+            cart.count = count
+
     cart.save()
 
     # 如果是ajax提交，直接返回json，否则转向购物车
     if request.is_ajax():
         count = CartInfo.objects.filter(user_id=request.session['user_id']).count()
-        # 当前用户购买了几个商品
+        # 当前用户购买了几种商品
         return JsonResponse({'count': count})
     else:
         return redirect('/cart/')
